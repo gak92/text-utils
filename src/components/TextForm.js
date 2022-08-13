ffb0031d69aca1a32d3a751e585a03dd58659cd1
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-14 03:18:49 +0500

    Add functionality to change the enter text into upper case

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,15 +3,24 @@ import PropTypes from 'prop-types';
 
 export default function TextForm(props) {
   const [text, setText] = useState("Enter the text here");
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  }
   
   return (
     <div className='container my-3'>
       <h1>{props.heading}</h1>
       <div className="mb-3">
-        <textarea className="form-control" id="myBox" rows="8" value={text}>
+        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleTextChange}>
         </textarea>
       </div>
-      <button className="btn btn-primary">
+      <button className="btn btn-primary" onClick={handleUpClick}>
         Convert Text to UpperCase
       </button>
     </div>
