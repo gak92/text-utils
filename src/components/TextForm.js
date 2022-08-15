59bb351538542152600ab894e3fd9a3e5d33a87d
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-16 02:39:26 +0500

    fix button gaps

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -54,19 +54,19 @@ export default function TextForm(props) {
           }}>
         </textarea>
       </div>
-      <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>
         Convert Text to UpperCase
       </button>
-      <button className="btn btn-primary mx-1" onClick={handleLoClick}>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>
         Convert Text to LowerCase
       </button>
-      <button className="btn btn-primary mx-1" onClick={handleClearClick}>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>
         Clear Text
       </button>
-      <button className="btn btn-primary mx-1" onClick={handleCopyClick}>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleCopyClick}>
         Copy Text
       </button>
-      <button className="btn btn-primary mx-1" onClick={handleRemoveExtraSpaces}>
+      <button className="btn btn-primary mx-1 my-1" onClick={handleRemoveExtraSpaces}>
         Remvoe Extra Spaces
       </button>
       <div className='my-3'>
